26166e55650c489ebd320b3ff55e52e3445f7cd5
DCID33 <davidcedrone@me.com>
2021-06-17 07:20:55 -0400

    update

---
+++ _admin_code/PROD.cmd
@@ -0,0 +1,9 @@
+cd \Users\DC\Documents
+"C:\Program Files\R\R-3.6.3\bin\RScript.exe" C:\Users\DC\Documents\Sites\SITE1\_r_code\PRODSITE.R
+
+xcopy /y /E "C:\Users\DC\Documents\Sites\SITE1" "C:\Users\DC\Documents\GitHub\SITE"
+
+cd \Users\DC\Documents\GitHub\SITE
+git add .
+git commit -m "update"
+git push
