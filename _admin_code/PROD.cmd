9fd158ee8d5379cb30fdea5bc7b5a75551c8641c
DCID33 <davidcedrone@me.com>
2021-08-06 17:50:13 -0400

    update

--- _admin_code/PROD.cmd
+++ _admin_code/PROD.cmd
@@ -2,7 +2,8 @@ cd \Users\DC\Documents
 "C:\Program Files\R\R-3.6.3\bin\RScript.exe" C:\Users\DC\Documents\Sites\SITE1\_r_code\PRODSITE.R
 "C:\Program Files\R\R-3.6.3\bin\RScript.exe" C:\Users\DC\Documents\Sites\SITE1\_r_code\valueboxes.R
 
-xcopy /y /E "C:\Users\DC\Documents\Sites\SITE1\_r_code\valueboxes.html" "C:\Users\DC\Documents\Sites\SITE1\valueboxes.html"
+xcopy /y "C:\Users\DC\Documents\Sites\SITE1\_r_code\valueboxes.html" "C:\Users\DC\Documents\Sites\SITE1\valueboxes.html"
+del "C:\Users\DC\Documents\Sites\SITE1\_r_code\valueboxes.html"
 xcopy /y /E "C:\Users\DC\Documents\Sites\SITE1" "C:\Users\DC\Documents\GitHub\SITE"
 
 cd \Users\DC\Documents\GitHub\SITE
--- _r_code/valueboxes.R
+++ _r_code/valueboxes.R
@@ -1,1 +1,3 @@
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 rmarkdown::render("~/Sites/SITE1/_r_code/valueboxes.Rmd")
+
